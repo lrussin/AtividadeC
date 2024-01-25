5128df85dc00e3b3fdc8819666811b4733cbd8ba
lucas russin <lucas.russin@atos.net>
2024-01-25 16:05:21 -0300

    Atividade2 Acelera .Net

--- obj/Debug/net8.0/Atividade2.AssemblyInfo.cs
+++ obj/Debug/net8.0/Atividade2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Atividade2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43405a75655583e2a12eedf29bdce62356193a83")]
 [assembly: System.Reflection.AssemblyProductAttribute("Atividade2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Atividade2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
